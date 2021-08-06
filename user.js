513087f4ef25cc5b4eae3abeb86c1fda3eaf5dc1
FuLygon <notfulygon.fgn@gmail.com>
2021-08-06 15:59:28 +0700

    Added config file, fixing some small stuff and cleaning up

--- user.js
+++ user.js
@@ -1,4 +1,4 @@
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // default: false
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // default: false, this need to be true
 user_pref("services.sync.addons.ignoreUserEnabledChanges", false); // set to true if you want to ignore addons sync
 user_pref("gfx.webrender.all", true);
 user_pref("gfx.webrender.enabled", true);
@@ -6,5 +6,5 @@ user_pref("ui.key.menuAccessKeyFocuses", false); // default: true
 user_pref("browser.tabs.closeWindowWithLastTab", false); // default: true
 user_pref("extensions.pocket.enabled", false); // default: true
 user_pref("layout.css.backdrop-filter.enabled", true); // default: false
-user_pref("layout.spellcheckDefault", 0);
 user_pref("svg.context-properties.content.enabled", true);
+user_pref("browser.tabs.drawInTitlebar", true);
